1b8ff197d7fc8f816d4e0502684c75eb03b403bf
Roland Haas <rhaas@mail.ubc.ca>
2026-04-13 13:45:01 -0700

    Boost: add more header files to detection
    tries to give one header file per library

--- src/detect.sh
+++ src/detect.sh
@@ -23,9 +23,9 @@ fi
 
 # Try to find the library if build isn't explicitly requested
 if [ -z "${BOOST_BUILD}" ]; then
-    # look for the same shared libraries David's Boost provided
-    # systems is include-only since Boost 1.69 which is from 2018
-    find_lib BOOST boost 1 1.0 "boost_filesystem" "boost/filesystem.hpp boost/system" "$BOOST_DIR"
+    # look for the same libraries David's Boost provided, skip libboost_system
+    # since it has been removed in new versions
+    find_lib BOOST boost 1 1.0 "boost_atomic boost_filesystem boost_math_c99 boost_math_c99f boost_math_c99l boost_math_tr1 boost_math_tr1f boost_math_tr1l" "boost/atomic.hpp boost/filesystem.hpp boost/math/constants/constants.hpp boost/math/tr1.hpp boost/system.hpp" "$BOOST_DIR"
 fi
 
 THORN=Boost
@@ -55,6 +55,7 @@ if [ -n "$BOOST_BUILD" -o -z "${BOOST_DIR}" ]; then
     BOOST_DIR=${INSTALL_DIR}
     BOOST_INC_DIRS="$BOOST_DIR/include"
     BOOST_LIB_DIRS="$BOOST_DIR/lib"
+    # we build libboost_system (still present in 1.84)
     BOOST_LIBS="boost_atomic boost_filesystem boost_math_c99 boost_math_c99f boost_math_c99l boost_math_tr1 boost_math_tr1f boost_math_tr1l boost_system"
 else
     BOOST_BUILD=
